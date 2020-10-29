f71e2a6c5cebd3cc41d1415b42b0255e97188032
GuYueyueHH <73649104+GuYueyueHH@users.noreply.github.com>
2020-10-29 21:51:49 +0800

    binder test2

---
+++ python_machinelearning_kaggle_practice/test.py
@@ -0,0 +1,11 @@
+print('test success!')
+
+import matplotlib.pyplot as plt
+x = list(range(100))
+y = list(range(100))
+plt.figure(1,figsize=([6,6]))
+plt.plot(x,y)
+plt.show()
+
+plt.figure(2,figsize=[10,6])
+import numpy as np
