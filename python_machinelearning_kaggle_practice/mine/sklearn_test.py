024e85272974d4cda95436ba46e22eea1bec67cd
GuYueyueHH <Icassysysy@gmail.com>
2020-10-31 03:50:52 +0800

    sklearn_test.py created

--- python_machinelearning_kaggle_practice/mine/sklearn_test.py
+++ python_machinelearning_kaggle_practice/mine/sklearn_test.py
@@ -228,7 +228,7 @@ print('Trees test end!')
 
 
 # %%
-print('Linear regression test begin!')
+print('Linear regression test begin!\n')
 # linear regression
 from sklearn.datasets import load_boston
 boston = load_boston()
@@ -244,8 +244,9 @@ ss_x = StandardScaler()
 x_train = ss_x.fit_transform(x_train)
 x_test = ss_x.transform(x_test)
 ss_y = StandardScaler()
-y_train = ss_y.fit_transform(y_train)
-y_test = ss_y.transform(y_test)
+y_train = ss_y.fit_transform(y_train.reshape(-1, 1))
+y_test = ss_y.transform(y_test.reshape(-1, 1))
+# print(y_train.shape)
 
 # 模型拟合
 # linear regression
@@ -270,13 +271,220 @@ ms_error_sgdr = mean_squared_error(ss_y.inverse_transform(y_test),ss_y.inverse_t
 # mean absolute error
 ma_error_lr = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_lr_predict))
 ma_error_sgdr = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_sgdr_predict))
-print('r2 score of linear regression:',r2_score_lr)
-print('r2 score of sgd regression:',r2_score_sgdr)
-print('mean squared error of linear regression:',ms_error_lr)
-print('mean squared error of sgd regression:',ms_error_sgdr)
-print('mean absolute error of linear regression:',ma_error_lr)
-print('mean absolute error of sgd regression:',ma_error_sgdr)
+print('r2 score of linear regression:\t\t\t%.4f'%r2_score_lr)
+print('r2 score of sgd regression:\t\t\t%.4f\n'%r2_score_sgdr)
+print('mean squared error of linear regression:\t%.4f'%ms_error_lr)
+print('mean squared error of sgd regression:\t\t%.4f\n'%ms_error_sgdr)
+print('mean absolute error of linear regression:\t%.4f'%ma_error_lr)
+print('mean absolute error of sgd regression:\t\t%.4f'%ma_error_sgdr)
 
-print('Linear regression test end!')
+print('\nLinear regression test end!')
+
+
+# %%
+print('SVR regression test begin!\n')
+# SVR regression
+from sklearn.datasets import load_boston
+boston = load_boston()
+# print(boston.DESCR)
+
+# cross validation
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.25,random_state=33)
+
+# 标准归一化处理
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+ss_y = StandardScaler()
+y_train = ss_y.fit_transform(y_train.reshape(-1, 1))
+y_test = ss_y.transform(y_test.reshape(-1, 1))
+# print(y_train.shape)
+
+# # 模型拟合
+# from sklearn.svm import SVR
+# # SVR kernel='linear'
+# svr_linear = SVR(kernel='linear')
+# svr_linear.fit(x_train,y_train)
+# y_svr_linear_predict = svr_linear.predict(x_test)
+# # SVR kernel='poly'
+# svr_poly = SVR(kernel='poly')
+# svr_poly.fit(x_train,y_train)
+# y_svr_poly_predict = svr_poly.predict(x_test)
+# # SVR kernel='rbf'
+# svr_rbf = SVR(kernel='rbf')
+# svr_rbf.fit(x_train,y_train)
+# y_svr_rbf_predict = svr_rbf.predict(x_test)
+
+# # 模型评估
+# from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+# # r2_score
+# r2_score_svr_linear = r2_score(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_linear_predict))
+# r2_score_svr_poly = r2_score(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_poly_predict))
+# r2_score_svr_rbf = r2_score(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_rbf_predict))
+# # mean squared error
+# ms_error_svr_linear = mean_squared_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_linear_predict))
+# ms_error_svr_poly = mean_squared_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_poly_predict))
+# ms_error_svr_rbf = mean_squared_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_rbf_predict))
+# # mean absolute error
+# ma_error_svr_linear = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_linear_predict))
+# ma_error_svr_poly = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_poly_predict))
+# ma_error_svr_rbf = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_svr_rbf_predict))
+# print('r2 score of SVR linear regression:\t\t\t%.4f'%r2_score_svr_linear)
+# print('r2 score of SVR poly regression:\t\t\t%.4f'%r2_score_svr_poly)
+# print('r2 score of SVR rbf regression:\t\t\t\t%.4f\n'%r2_score_svr_rbf)
+
+# print('mean squared error of SVR linear regression:\t\t%.4f'%ms_error_svr_linear)
+# print('mean squared error of SVR poly regression:\t\t%.4f'%ms_error_svr_poly)
+# print('mean squared error of SVR rbf regression:\t\t%.4f\n'%ms_error_svr_rbf)
+
+# print('mean absolute error of SVR linear regression:\t\t%.4f'%ma_error_svr_linear)
+# print('mean absolute error of SVR poly regression:\t\t%.4f'%ma_error_svr_poly)
+# print('mean absolute error of SVR rbf regression:\t\t%.4f'%ma_error_svr_rbf)
+
+from sklearn.svm import SVR
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+model_data = {'svr_ins':{},'y_predict':{},'r2_score':{},'ms_error':{},'ma_error':{}}
+kernel_list = ['linear','poly','rbf']
+for svr_kernel in kernel_list:
+    model_data['svr_ins'][svr_kernel] =  SVR(kernel=svr_kernel)
+    model_data['svr_ins'][svr_kernel].fit(x_train,y_train)
+    model_data['y_predict'][svr_kernel] = model_data['svr_ins'][svr_kernel].predict(x_test)
+    model_data['r2_score'][svr_kernel] = r2_score(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][svr_kernel]))
+    model_data['ms_error'][svr_kernel] = mean_squared_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][svr_kernel]))
+    model_data['ma_error'][svr_kernel] = mean_absolute_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][svr_kernel]))
+score_list = ['r2_score','ms_error','ma_error']
+for svr_score in score_list:
+    print()
+    for svr_kernel in kernel_list:
+        print('%8s of SVR %6s regression:\t%.4f'%(svr_score, svr_kernel, model_data[svr_score][svr_kernel]))
+
+
+
+print('\nSVR regression test end!')
+
+
+# %%
+print('KNN regression test begin!\n')
+# KNN regression
+from sklearn.datasets import load_boston
+boston = load_boston()
+# print(boston.DESCR)
+
+# cross validation
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.25,random_state=33)
+
+# 标准归一化处理
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+ss_y = StandardScaler()
+y_train = ss_y.fit_transform(y_train.reshape(-1, 1))
+y_test = ss_y.transform(y_test.reshape(-1, 1))
+# print(y_train.shape)
+
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+model_data = {'KNN_ins':{},'y_predict':{},'r2_score':{},'ms_error':{},'ma_error':{}}
+weights_list = ['uniform','distance']
+for KNN_weight in weights_list:
+    model_data['KNN_ins'][KNN_weight] =  KNeighborsRegressor(weights=KNN_weight)
+    model_data['KNN_ins'][KNN_weight].fit(x_train,y_train)
+    model_data['y_predict'][KNN_weight] = model_data['KNN_ins'][KNN_weight].predict(x_test)
+    model_data['r2_score'][KNN_weight] = r2_score(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][KNN_weight]))
+    model_data['ms_error'][KNN_weight] = mean_squared_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][KNN_weight]))
+    model_data['ma_error'][KNN_weight] = mean_absolute_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][KNN_weight]))
+score_list = ['r2_score','ms_error','ma_error']
+for KNN_score in score_list:
+    print()
+    for KNN_weight in weights_list:
+        print('%8s of KNN %8s regression:\t%2.4f'%(KNN_score, KNN_weight, model_data[KNN_score][KNN_weight]))
+
+print('\nKNN regression test end!')
+
+
+# %%
+print('TREE regression test begin!\n')
+# TREE regression
+from sklearn.datasets import load_boston
+boston = load_boston()
+# print(boston.DESCR)
+
+# cross validation
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.25,random_state=33)
+
+# 标准归一化处理
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+ss_y = StandardScaler()
+y_train = ss_y.fit_transform(y_train.reshape(-1, 1))
+y_test = ss_y.transform(y_test.reshape(-1, 1))
+# print(y_train.shape)
+
+# 模型拟合
+from sklearn.tree import DecisionTreeRegressor
+dtr = DecisionTreeRegressor()
+dtr.fit(x_train,y_train)
+y_predict = dtr.predict(x_test)
+
+# 模型评估
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+# r2_score
+r2_score = r2_score(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_predict))
+# mean squared error
+ms_error = mean_squared_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_predict))
+# mean absolute error
+ma_error = mean_absolute_error(ss_y.inverse_transform(y_test),ss_y.inverse_transform(y_predict))
+print('r2 score            of Decision TREE regression:%.4f'%r2_score)
+print('mean squared error  of Decision TREE regression:%.4f'%ms_error)
+print('mean absolute error of Decision TREE regression:%.4f'%ma_error)
+print('\nTREE regression test end!')
+
+
+# %%
+print('Ensemble regression test begin!\n')
+# Ensemble regression
+from sklearn.datasets import load_boston
+boston = load_boston()
+# print(boston.DESCR)
+
+# cross validation
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.25,random_state=33)
+
+# 标准归一化处理
+from sklearn.preprocessing import StandardScaler
+ss_x = StandardScaler()
+x_train = ss_x.fit_transform(x_train)
+x_test = ss_x.transform(x_test)
+ss_y = StandardScaler()
+y_train = ss_y.fit_transform(y_train.reshape(-1, 1))
+y_test = ss_y.transform(y_test.reshape(-1, 1))
+# print(y_train.shape)
+
+from sklearn.ensemble import RandomForestRegressor,ExtraTreesRegressor,GradientBoostingRegressor
+from sklearn.metrics import r2_score,mean_squared_error,mean_absolute_error
+model_data = {'model':{},'y_predict':{},'r2_score':{},'ms_error':{},'ma_error':{}}
+model_list = ['RandomForestRegressor','ExtraTreesRegressor','GradientBoostingRegressor']
+for ensemble_model in model_list:
+    model_data['model'][ensemble_model] = eval(ensemble_model)()
+    model_data['model'][ensemble_model].fit(x_train,y_train)
+    model_data['y_predict'][ensemble_model] = model_data['model'][ensemble_model].predict(x_test)
+    model_data['r2_score'][ensemble_model] = r2_score(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][ensemble_model]))
+    model_data['ms_error'][ensemble_model] = mean_squared_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][ensemble_model]))
+    model_data['ma_error'][ensemble_model] = mean_absolute_error(ss_y.inverse_transform(y_test), ss_y.inverse_transform(model_data['y_predict'][ensemble_model]))
+score_list = ['r2_score','ms_error','ma_error']
+for ensemble_score in score_list:
+    print()
+    for ensemble_model in model_list:
+        print('%8s of ensemble %26s regression:\t%2.4f'%(ensemble_score, ensemble_model, model_data[ensemble_score][ensemble_model]))
+
+print('\nEnsemble regression test end!')
 
 
